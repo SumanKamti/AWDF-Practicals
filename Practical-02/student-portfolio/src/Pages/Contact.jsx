397963f2859a6a7936bde325cbaf2e054705b680
Suman <sumankamti684@gmail.com>
2026-07-19 19:40:13 +0530

    Add Projects and Contact pages

--- Practical-02/student-portfolio/src/Pages/Contact.jsx
+++ Practical-02/student-portfolio/src/Pages/Contact.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
+
 function Contact() {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <div className="card">
-      <h2>Contact</h2>
 
-      <p>Email : your@email.com</p>
+      <h2>Contact Me</h2>
+
+      <input
+        type="text"
+        placeholder="Enter Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="email"
+        placeholder="Enter Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <br /><br />
+
+      <h3>Preview</h3>
+
+      <p>Name : {name}</p>
+
+      <p>Email : {email}</p>
+
     </div>
   );
 }
